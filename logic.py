9309e88ad3d9c86a9b4999dba186c6effad27f50
Chris Pressland <mail@cpressland.io>
2022-07-21 17:46:00 +0100

    More descriptive prints

--- logic.py
+++ logic.py
@@ -9,40 +9,40 @@ def move_logic(body, height, width):
         try:
             if body_chunk["x"] == head["x"] - 1:
                 options.remove("left")
-                print("Removed Left")
+                print("Cannibalism: Removed Left")
             elif body_chunk["x"] == head["x"] + 1:
                 options.remove("right")
-                print("Removed Right")
+                print("Cannibalism: Removed Right")
             elif body_chunk["y"] == head["y"] - 1:
                 options.remove("down")
-                print("Removed Down")
+                print("Cannibalism: Removed Down")
             elif body_chunk["y"] == head["y"] + 1:
                 options.remove("up")
-                print("Removed Up")
+                print("Cannibalism: Removed Up")
         except ValueError:
             continue
     try:
         if head["y"] == 0:
             options.remove("down")
-            print("Removed Down")
+            print("Wall: Removed Down")
     except ValueError:
         pass
     try:
         if head["y"] == height - 1:
             options.remove("up")
-            print("Removed Up")
+            print("Wall: Removed Up")
     except ValueError:
         pass
     try:
         if head["x"] == 0:
             options.remove("left")
-            print("Removed Left")
+            print("Wall: Removed Left")
     except ValueError:
         pass
     try:
         if head["x"] == width - 1:
             options.remove("right")
-            print("Removed Right")
+            print("Wall: Removed Right")
     except ValueError:
         pass
     return random.choice(options)
