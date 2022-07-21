5a198fcccb34a62d59e24300d7c892a8a2aa453f
Chris Pressland <mail@cpressland.io>
2022-07-21 17:17:06 +0100

    Maybe one less, or one more... maybe maybe

--- logic.py
+++ logic.py
@@ -18,13 +18,13 @@ def move_logic(body, height, width):
         except ValueError:
             continue
     try:
-        if head["y"] == 1:
+        if head["y"] <= 1:
             options.remove("down")
-        elif head["y"] == height - 1:
+        elif head["y"] >= height - 1:
             options.remove("up")
-        elif head["x"] == 1:
+        elif head["x"] <= 1:
             options.remove("left")
-        elif head["x"] == width - 1:
+        elif head["x"] >= width - 1:
             options.remove("right")
     except ValueError:
         pass
