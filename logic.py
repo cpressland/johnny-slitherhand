a8c75b6c5346ee432c26c56594fd807ca3e3d7b0
Chris Pressland <mail@cpressland.io>
2022-07-21 16:37:31 +0100

    Further cannibalism fixes

--- logic.py
+++ logic.py
@@ -4,13 +4,14 @@ import random
 def move_logic(body):
     options = ["up", "down", "left", "right"]
     head = body[0]
-    neck = body[1]
-    if neck["x"] < head["x"]:
-        options.remove("left")
-    elif neck["x"] > head["x"]:
-        options.remove("right")
-    elif neck["y"] < head["y"]:
-        options.remove("down")
-    elif neck["y"] > head["y"]:
-        options.remove("up")
+    torso = body[1:]
+    for body_chunk in torso:
+        if body_chunk["x"] == head["x"]-1:
+            options.remove("left")
+        elif body_chunk["x"] == head["x"]+1:
+            options.remove("right")
+        elif body_chunk["y"] == head["y"]-1:
+            options.remove("down")
+        elif body_chunk["y"] == head["y"]+1:
+            options.remove("up")
     return random.choice(options)
